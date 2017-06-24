f55d60db61f467051aa0ec095d6fc204ed56a66d
el-tocino <email+github@aforkinthetoad.com>
2017-06-24 01:30:02 -0500

    last 10 minutes graph.

--- Plots.py
+++ Plots.py
@@ -1,3 +1,5 @@
+import time
+import datetime
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -10,20 +12,22 @@ sensorsql = "select name, sensor from sensors"
 cur.execute(sensorsql)
 sensors = cur.fetchall()
 
-
 for names in sensors:
+    x_val = [] ;
+    y_val = [] ;
     sensor = str(names[1])
     sensorname = str(names[0])
     sql = "SELECT time , temperature from temperatures where sensor=%s order by time desc limit 40"
     cur.execute(sql, sensor)
     data = cur.fetchall()
     for linez in data:
-        print (str(linez[0]),float(linez[1]))
-
-    x_val = [str(x[0]) for x in linez]
-    y_val = [float(y[1]) for y in linez]
-    plt.plot(x_val,y_val, label='sensorname')
-    print "done plotting!"
+        the_date = str(linez[0])
+        x_val.append(time.mktime(datetime.datetime.strptime(the_date, "%Y-%m-%d %H:%M:%S").timetuple()));
+        y_val.append(float(linez[-1]));
+        print (the_date, x_val[-1], y_val[-1])
+     
+    plt.plot(x_val,y_val, label=sensorname)
+    print "done plotting %s!",sensorname
 
 conn.close
 
@@ -31,3 +35,4 @@ plt.xlabel('Time')
 plt.ylabel('Temperature')
 plt.legend()
 plt.savefig('foo.png')
+
