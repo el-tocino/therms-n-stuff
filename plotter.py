b032e4e6439f98eff416db3cec7de89fd8534885
el-tocino <email+git@aforkinthetoad.com>
2017-06-24 22:39:15 +0000

    swapped for the more usable version.

--- plotter.py
+++ plotter.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import datetime
 import matplotlib
@@ -12,26 +13,32 @@ sensorsql = "select name, sensor from sensors"
 cur.execute(sensorsql)
 sensors = cur.fetchall()
 
+tempcount = int(sys.argv[1])
+pngfile = sys.argv[2]
+
 for names in sensors:
     x_val = [] ;
     y_val = [] ;
     sensor = str(names[1])
     sensorname = str(names[0])
-    sql = "SELECT time , temperature from temperatures where sensor=%s order by time desc limit 40"
-    cur.execute(sql, sensor)
+    sql = "SELECT time , temperature from temperatures where sensor=%s order by time desc limit %s"
+    cur.execute(sql, (sensor,tempcount))
     data = cur.fetchall()
     for linez in data:
         the_date = str(linez[0])
         x_val.append(time.mktime(datetime.datetime.strptime(the_date, "%Y-%m-%d %H:%M:%S").timetuple()));
         y_val.append(float(linez[-1]));
-        print (the_date, x_val[-1], y_val[-1])
+        #print (the_date, x_val[-1], y_val[-1])
      
     plt.plot(x_val,y_val, label=sensorname)
-    print "done plotting %s!",sensorname
+    #print "done plotting %s!",sensorname
 
 conn.close
 
-plt.xlabel('Time')
+x_label=time.ctime()
+
+plt.xticks([])
+plt.xlabel(x_label)
 plt.ylabel('Temperature')
 plt.legend()
-plt.savefig('foo.png')
+plt.savefig(pngfile)
